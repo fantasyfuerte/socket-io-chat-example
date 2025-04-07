27b91e7c1c40cc4f4992b178b669e0cd631c5876
fuerte <leandrofuerte75@gmail.com>
2025-04-07 10:45:26 +0100

    adding message event

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ io.on("connection", (socket) => {
   });
 });
 
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
 server.listen(3000, () => {
   console.log("server running at http://localhost:3000");
 });
